97baebd5c97fd1460c64748cef6916a824a00942
Matheus Marciano <mm.leitea@outlook.com>
2025-05-15 13:28:07 +0000

    🎉 Create React + Vite project with mocha test lib

---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import stylisticTs from '@stylistic/eslint-plugin-ts'
+import stylisticJsx from '@stylistic/eslint-plugin-jsx'
+
+export default tseslint.config(
+	{ ignores: ['dist'] },
+	{
+		extends: [js.configs.recommended, ...tseslint.configs.recommended],
+		files: ['**/*.{ts,tsx}'],
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: globals.browser,
+		},
+		plugins: {
+			'react-hooks': reactHooks,
+			'react-refresh': reactRefresh,
+			'@stylistic/ts': stylisticTs,
+			'@stylistic/jsx': stylisticJsx,
+		},
+		rules: {
+			...reactHooks.configs.recommended.rules,
+			'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+			'@stylistic/ts/semi': ['error', 'never'],
+			'@stylistic/ts/indent': ['error', 'tab'],
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{
+					argsIgnorePattern: '^_',
+					varsIgnorePattern: '^_',
+					caughtErrorsIgnorePattern: '^_',
+				},
+			],
+			'@stylistic/ts/comma-dangle': [
+				'error',
+				{
+					arrays: 'always-multiline',
+					objects: 'always-multiline',
+					imports: 'always-multiline',
+					exports: 'always-multiline',
+					functions: 'never',
+				},
+			],
+		},
+	}
+)
---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+export default {
+	semi: false,
+	useTabs: true,
+	tabWidth: 2,
+	singleQuote: true,
+	printWidth: 100,
+	trailingComma: 'es5',
+}
