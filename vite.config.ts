eeb8325bd2a8e674e75cabdaba05cb7453f7d850
Matheus Marciano <mm.leitea@outlook.com>
2025-06-02 17:22:09 +0000

    ➕ Add unplugin dependencies + three

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,36 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
 	const INITIAL_DATE = mode !== 'production' ? '24 Dec 1997 12:00:15 GMT' : ''
 	return {
-		plugins: [vue(), vueDevTools()],
+		plugins: [
+			vue(),
+			vueDevTools(),
+			AutoImport({
+				imports: [
+					'vue',
+					{
+						'vue-router/auto': ['useRoute', 'useRouter'],
+					},
+				],
+				dts: 'src/auto-imports.d.ts',
+				eslintrc: {
+					enabled: true,
+				},
+				vueTemplate: true,
+			}),
+			Components({
+				dts: 'src/components.d.ts',
+			}),
+		],
 		resolve: {
 			alias: {
 				'@': fileURLToPath(new URL('./src', import.meta.url)),
