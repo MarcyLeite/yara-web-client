4c61d5b6d1396a9e1e87707e0c72a8988b1b5940
Matheus Marciano <mm.leitea@outlook.com>
2025-06-02 17:21:16 +0000

    💄 Add css composition and yara styles

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,14 @@
-<script setup lang="ts">
-import Yara from './components/Yara.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-	<header>
-		<img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-		<div class="wrapper"></div>
-	</header>
-
-	<main>
-		<Yara />
+	<main class="yara-wrapper">
+		<yara />
 	</main>
 </template>
 
 <style scoped>
-header {
-	line-height: 1.5;
-}
-
-.logo {
-	display: block;
-	margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-	header {
-		display: flex;
-		place-items: center;
-		padding-right: calc(var(--section-gap) / 2);
-	}
-
-	.logo {
-		margin: 0 2rem 0 0;
-	}
-
-	header .wrapper {
-		display: flex;
-		place-items: flex-start;
-		flex-wrap: wrap;
-	}
+.yara-wrapper {
+	width: 100%;
+	height: 100%;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import './assets/main.css'
-
+import '@/assets/style/app.scss'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
 import App from './App.vue'
 
 const app = createApp(App)
