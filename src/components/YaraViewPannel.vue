c5f1b45c42fe6a601d5842803fc76b94ca713bd4
Matheus Marciano <mm.leitea@outlook.com>
2025-06-24 14:09:13 +0000

    🚑️ Change hidden component position and update to only show hidden

---
+++ src/components/YaraViewPannel.vue
@@ -0,0 +1,20 @@
+<template>
+	<y-pannel>
+		<div class="pa-2 d-flex flex-column ga-4">
+			<view-selector :store="store" />
+			<y-pannel>
+				<y-hidden-list :store="store" />
+			</y-pannel>
+		</div>
+	</y-pannel>
+</template>
+
+<script setup lang="ts">
+import type { YaraStore } from '@/stores/yara-store'
+
+type Props = {
+	store: YaraStore
+}
+
+const { store } = defineProps<Props>()
+</script>
