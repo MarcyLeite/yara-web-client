5ae2f2be1c710309823cfca6c89d1143eabcd09b
Matheus Marciano <mm.leitea@outlook.com>
2025-06-27 17:25:14 +0000

    🚑️ Fix object visibility not toggling on show/hide press

--- src/components/YHiddenList.vue
+++ src/components/YHiddenList.vue
@@ -4,17 +4,20 @@
 			<span class="text-subtitle-1 text-bold">Hidden Components</span>
 		</div>
 
-		<div
-			class="px-4 d-flex flex-column text-subtitle-2"
-			style="max-height: 20rem; overflow: scroll"
-		>
-			<div class="text-light-alpha-50" v-if="yara3DState.hiddenList?.length === 0">None</div>
-			<div v-else v-for="(objectName, i) in yara3DState.hiddenList" :key="i">
+		<div class="px-4 d-flex flex-column text-subtitle-2" style="height: 15rem; overflow: scroll">
+			<div v-for="(objectName, i) in yara3DState.hiddenList" :key="i">
 				<y-divider v-if="i > 0" />
 				<div class="py-2 d-flex align-center justify-space-between ga-2">
 					<div class="text-truncate">{{ objectName }}</div>
 
-					<y-btn type="flat" @click="() => {}">
+					<y-btn
+						type="flat"
+						@click="
+							() => {
+								store.yara3D?.toggleObjectVisible(objectName, true)
+							}
+						"
+					>
 						<div>show</div>
 					</y-btn>
 				</div>
